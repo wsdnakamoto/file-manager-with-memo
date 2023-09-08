78987954e73eecfff16c820eb2f9dbec426281ad
wsdnakamoto <wsdnakamoto@gmail.com>
2023-09-08 23:46:40 +0900

    created folder structure

---
+++ src/main/java/com/example/demo/SchoolStoreManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolStoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolStoreManagerApplication.class, args);
+	}
+
+}
