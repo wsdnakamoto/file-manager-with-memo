8dedc1bf29f5707bdd66f09e2e16ee57fedc8fd4
wsdnakamoto <wsdnakamoto@gmail.com>
2023-09-12 05:08:06 +0900

    changed application.properties to use aws parameter store, RDS and S3

--- src/main/java/com/example/demo/FileManagerWithMemoApplication.java
+++ src/main/java/com/example/demo/FileManagerWithMemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolStoreManagerApplication {
+public class FileManagerWithMemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolStoreManagerApplication.class, args);
+		SpringApplication.run(FileManagerWithMemoApplication.class, args);
 	}
 
 }
