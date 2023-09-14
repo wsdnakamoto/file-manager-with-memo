c787f3f9c84fecefd86ee0717c89a77644852b75
wsdnakamoto <wsdnakamoto@gmail.com>
2023-09-14 13:37:15 +0900

    made the mapper

--- src/main/java/com/example/demo/domain/File.java
+++ src/main/java/com/example/demo/domain/File.java
@@ -1,5 +1,19 @@
 package com.example.demo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.repository.FileMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Transactional
 public class File {
 	
+	private final FileMapper mapper;
+	@Id
+	private int id;
+	private String name;
+	private String note;
 }
--- src/main/java/com/example/demo/repository/FileMapper.java
+++ src/main/java/com/example/demo/repository/FileMapper.java
@@ -1,8 +1,21 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.domain.File;
 
 @Mapper
 public interface FileMapper {
 	//CRUD操作のメソッド名だけを記述。
+	int insert(File record);
+	
+	File selectByPrimaryKey(int id);
+	
+	List<File> selectAll();
+	
+	int update(File record);
+	
+	int deleteByPrimaryKey(int id);
 }
