572a10c199dbbde10d90e94f277d65074a948eeb
wsdnakamoto <wsdnakamoto@gmail.com>
2023-09-26 16:28:13 +0900

    added the feature to upload file

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,5 +1,39 @@
 package com.example.demo.controller;
 
-public class FileController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.domain.File;
+import com.example.demo.service.FileService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class FileController {
+	
+	private FileService fileService;
+	
+	@GetMapping("/list")
+	public String getFileList() {
+		return "fileList";
+	}
+	
+	@GetMapping("/form")
+	public String form(@ModelAttribute File file) {
+		return "form";
+	}
+	
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("uploadFile") MultipartFile uploadFile, @ModelAttribute File file) {
+		file.setFileName(uploadFile);
+		file.setFilePath(uploadFile);
+		fileService.insertFile(file);
+		fileService.upload(uploadFile);
+		return "redirect:/list";
+	}
 }
--- src/main/java/com/example/demo/service/FileService.java
+++ src/main/java/com/example/demo/service/FileService.java
@@ -1,68 +1,95 @@
 package com.example.demo.service;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.example.demo.config.AwsS3Properties;
 import com.example.demo.domain.File;
 import com.example.demo.repository.FileMapper;
 
-import lombok.RequiredArgsConstructor;
+import io.awspring.cloud.core.io.s3.PathMatchingSimpleStorageResourcePatternResolver;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
 @Slf4j
 public class FileService {
 	
-	private final FileMapper mapper;
+	//コンストラクタインジェクション
+	private final FileMapper fileMapper;
 	
 	private final ResourceLoader resourceLoader;
 	
-	private final ResourcePatternResolver resourcePatternResolver;
-	
 	private final AwsS3Properties awsS3Properties;
 	
+	public FileService(FileMapper fileMapper, ResourceLoader resourceLoader, AwsS3Properties awsS3Properties) {
+		this.fileMapper = fileMapper;
+		this.resourceLoader = resourceLoader;
+		this.awsS3Properties = awsS3Properties;
+	}
+	
+	//セッターインジェクション
+	private  ResourcePatternResolver resourcePatternResolver;
+	
+	@Autowired
+	public void setupResolver(ApplicationContext applicationContext, AmazonS3 amazonS3) {
+		this.resourcePatternResolver = new PathMatchingSimpleStorageResourcePatternResolver(amazonS3, applicationContext);
+	}
 	
 	
 	public void insertFile(File file) {
-		mapper.insert(file);
+		fileMapper.insert(file);
 	}
 	
-	public File selectByPrimaryKey(int id) {
-		return mapper.selectByPrimaryKey(id);
+	public File selectByFileName(String fileName) {
+		return fileMapper.selectByFileName(fileName);
 	}
 	
-	public List<File> selectAll(){
-		return mapper.selectAll();
+	public List<File> selectAll() {
+		return fileMapper.selectAll();
 	}
 	
-	public void updateFile(File file) {
-		mapper.update(file);
+	public void deleteByFileName(String fileName) {
+		fileMapper.deleteByFileName(fileName);
 	}
 	
-	public void deleteByPrimaryKey(int id) {
-		mapper.deleteByPrimaryKey(id);
-	}
-	
-	public List<Resource> fileList(String fileName){
+	public List<Resource> fileList(String fileName) {
 		if(fileName == null) fileName = "";
-		
 		List<Resource> resourceList = null;
 		try {
 			Resource[] resourceArray = resourcePatternResolver.getResources("s3://"+awsS3Properties.getBucketName()+"/**/*"+fileName+"*.*");
 			resourceList = Arrays.asList(resourceArray);
 		}catch(IOException e) {
-			log.error("S3FileListError", e);
+			log.error("FileListError", e);
 		}
 		return resourceList;
 	}
+	
+	public void upload(MultipartFile uploadFile) {
+		Resource resource = this.resourceLoader.getResource("s3://"+awsS3Properties.getBucketName()+"/"+uploadFile.getOriginalFilename());
+		WritableResource writableResource = (WritableResource)resource;
+		try(OutputStream outputStream = writableResource.getOutputStream()){
+			outputStream.write(uploadFile.getBytes());
+		}catch(IOException e) {
+			log.error("FileUploadError", e);
+		}
+	}
+	
+	//public InputStream download(String fileName) throws IOException {
+	//	Resource resource = this.resourceLoader.getResource("s3://"+awsS3Properties.getBucketName()+"/"+fileName);
+	//	return resource.getInputStream();
+	//}
 }
